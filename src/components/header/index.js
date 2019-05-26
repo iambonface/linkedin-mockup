0f3349698642fb8163e31d716f5c3465b2c54197
Bonface Mudiri <mudirib@gmail.com>
2019-05-26 11:33:19 -0700

    Create basic header and nav links

---
+++ src/components/header/index.js
@@ -0,0 +1,81 @@
+import React from 'react';
+import StyledHeader from './styles.js';
+import logo from '../../logo.png';
+import Home from '@material-ui/icons/Home';
+import People from '@material-ui/icons/PeopleOutlineTwoTone';
+import Jobs from '@material-ui/icons/CardTravel';
+import Notifications from '@material-ui/icons/Notifications';
+import Comment from '@material-ui/icons/Comment';
+import Lens from '@material-ui/icons/Lens';
+import OndemandVideo from '@material-ui/icons/OndemandVideo';
+import Apps from '@material-ui/icons/Apps';
+
+
+class Header extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            avator: 'https://media.licdn.com/dms/image/C5603AQFcIplOECdFRw/profile-displayphoto-shrink_100_100/0?e=1564617600&v=beta&t=6Cvs5oOmi_Tid5bqrB5KfupaOapPRMtQlkIuUTVou6Q',
+
+        }
+    }
+    render() {
+        return (
+            <StyledHeader>
+                <div className="bar">
+                   <div className="logo">
+                       <img src={ logo } className="app-logo" alt="Logo" />
+                   </div>
+                   <div className="search">
+                       <input className="input-search" type="text" placeholder="&#x1F50D; Search" />
+                   </div>
+                   <div className="nav">
+                       <ul>
+                           <li className="home">
+                              <div className="badge-home">
+                                <Lens className="lens" />
+                              </div>
+                              <Home className="icon-home"/>
+                              <span><a href="/home">Home</a></span>
+                            </li>
+                            <li className="my-network">
+                              <People className="icon-people" />
+                              <span><a href="/home">My Network</a></span>
+                            </li>
+                            <li className="jobs">
+                              <Jobs className="icon-jobs" />
+                              <span><a href="/home">Jobs</a></span>
+                            </li>
+                            <li className="messaging">
+                              <Comment className="icon-comment"/>
+                              <span><a href="/home">Messaging</a></span>
+                            </li>
+                            <li className="notifications">
+                              <Notifications className="icon-notifications" />
+                              <span><a href="/home">Notifications</a></span>
+                            </li>
+                            <li>
+                                <img src={this.state.avator} alt="avator"  className="avator"/>
+                              <span><a href="/home">Me</a></span>
+                            </li>
+                        </ul>
+                   </div>
+                   <div className="actions">
+                    <ul>
+                            <li className="apps">
+                              <Apps className="icon-apps"/>
+                              <span><a href="/home">Work</a></span>
+                            </li>
+                            <li className="learning">
+                              <OndemandVideo className="icon-ondemandvideo"/>
+                              <span><a href="/home">Learning</a></span>
+                            </li>
+                    </ul>
+                   </div>
+                </div>
+            </StyledHeader>
+        );
+    }
+}
+
+export default Header;
---
+++ src/components/header/styles.js
@@ -0,0 +1,146 @@
+import styled from 'styled-components';
+const StyledHeader = styled.div`
+    .bar {
+        display: flex;
+        background-color: #283e4a;
+        color: #ffffff;
+        align-items: center;
+        padding-left: 68px;
+        padding-right: 68px;
+        height: 55px;
+    }
+    .logo {
+        padding: 5px 10px;
+    }
+    .app-logo {
+        width: 34px;
+        height: 34px;
+    }
+    .input-search {
+        padding: 6px;
+        font-size: 14px;
+        border: none;
+        border-radius: 3px;
+        background-color: rgb(225, 233, 238);
+        padding: 8px;
+        width: 230px;
+        margin: 2px;
+    }
+    .nav {
+        margin-left: auto;
+        font-size: 12px;
+        ul {
+            display: flex;
+            list-style: none;
+        }
+        a {
+            display: inline-block;
+            color: #CDD0D0;
+            margin: 2px;
+            text-decoration: none;
+        }
+
+        .fa {
+            color: #CDD0D0;
+        }
+        span {
+            display: block;
+        }
+        li {
+            text-align: center;
+            padding: 5px 10px;
+        }
+        li:last-child {
+            border-right: 1px solid rgb(92, 111, 124);
+            padding-right: 40px;
+        }
+    }
+    .actions {
+        font-size: 12px;
+        ul {
+            display: flex;
+            list-style: none;
+        }
+        span {
+            display: block;
+        }
+        li {
+            text-align: center;
+            padding: 5px 10px;
+            color: #CDD0D0;
+        }
+        a {
+            text-decoration: none;
+            color: #CDD0D0;
+        }
+    }
+    .avator {
+        border-width: 1px solid rgb(199, 209, 216);
+        border-radius: 50%;
+        height: 24px;
+        width: 24px;
+        cursor: pointer;
+    }
+    .home {
+        .badge-home {
+            position: absolute;
+            color: #ffffff;
+            margin-left: 18px;
+            background-color: rgb(209, 17, 36);
+            border-radius: 50%;
+            font-size: 14px;
+            width: 14px;
+            height: 14px;
+            border: 2px solid rgb(40, 62, 74);
+        }
+        .lens {
+            font-size: 4px;
+            padding: 5px;
+        }
+        .icon-home {
+            font-size: 26px;
+        }
+    }
+    .my-network {
+        .icon-people {
+            font-size: 26px;
+        }
+    }
+
+    .jobs {
+        .icon-jobs {
+            font-size: 26px;
+            color: #CDD0D0;
+        }
+    }
+
+    .messaging {
+        .icon-comment {
+            font-size: 26px;
+            color: #CDD0D0;
+        }
+    }
+    .notifications {
+        .icon-notifications {
+            font-size: 26px;
+            color: #CDD0D0;
+        }
+    }
+
+    .apps {
+        .icon-apps {
+            font-size: 26px;
+            color: #CDD0D0;
+        }
+    }
+
+    .learning {
+        .icon-ondemandvideo {
+            font-size: 26px;
+            color: #CDD0D0;
+        }
+    }
+
+`;
+
+export default StyledHeader;
